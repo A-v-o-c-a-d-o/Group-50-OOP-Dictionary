a15a6b1136b0362a56df26945b3f8969155015fd
nothingg24 <41933005+nothingg24@users.noreply.github.com>
2021-11-17 16:45:29 +0700

    add Add

---
+++ src/Add/Add.java
@@ -0,0 +1,42 @@
+package Controllers;
+
+import DictionaryCMD.DictionaryManagement;
+import DictionaryCMD.Word;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class Add implements Initializable {
+    @FXML
+    private TextField addTarget;
+    @FXML
+    private TextArea addExplain;
+    @FXML
+    private TextField addPhonetic;
+
+    public void save(ActionEvent actionEvent) throws SQLException {
+        String target = addTarget.getText().toLowerCase().trim();
+        String explain = addExplain.getText().toLowerCase().trim();
+        String pronounce = addPhonetic.getText().toLowerCase().trim();
+        if (!target.equals("") && !explain.equals("")) {
+            Word selectedWord = new Word();
+            selectedWord.setWord_target(target);
+            selectedWord.setWord_explain(explain);
+            selectedWord.setWord_phonetics(pronounce);
+            DictionaryManagement.insertToDatabase(selectedWord);
+        }
+        addTarget.clear();
+        addExplain.clear();
+        addPhonetic.clear();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
+}
