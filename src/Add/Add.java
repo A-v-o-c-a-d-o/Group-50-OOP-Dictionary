156686e623464c42d2bde054a3fe245a6ef292ab
nothingg24 <41933005+nothingg24@users.noreply.github.com>
2021-11-19 21:47:13 +0700

    change Add

--- src/Add/Add.java
+++ src/Add/Add.java
@@ -1,4 +1,4 @@
-package Add;
+package Controllers;
 
 import DictionaryCMD.DictionaryManagement;
 import DictionaryCMD.Word;
@@ -7,11 +7,17 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class Add implements Initializable {
+    @FXML
+    private ImageView canAdd;
+    @FXML
+    private ImageView cantAdd;
     @FXML
     private TextField addTarget;
     @FXML
@@ -23,21 +29,26 @@ public class Add implements Initializable {
         String target = addTarget.getText().toLowerCase().trim();
         String explain = addExplain.getText().toLowerCase().trim();
         String pronounce = addPhonetic.getText().toLowerCase().trim();
-
         if (!target.equals("") && !explain.equals("") && !DictionaryManagement.includeDB(target)) {
             Word selectedWord = new Word();
             selectedWord.setWord_target(target);
             selectedWord.setWord_explain(explain);
             selectedWord.setWord_phonetics(pronounce);
             DictionaryManagement.insertToDatabase(selectedWord);
+            canAdd.setVisible(true);
+            cantAdd.setVisible(false);
+        } else {
+            cantAdd.setVisible(true);
+            canAdd.setVisible(false);
         }
         addTarget.clear();
         addExplain.clear();
         addPhonetic.clear();
-
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        canAdd.setVisible(false);
+        cantAdd.setVisible(false);
     }
 }
